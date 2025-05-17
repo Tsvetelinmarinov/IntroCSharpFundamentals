71bbb8cd55f31744f76c1094f11311f222c2c29b
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-05-17 14:49:42 +0300

    Задача 12

---
+++ 12.EvenNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * 12. Even Number
+ */
+
+using System;
+
+namespace _12.EvenNumber;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        //Create a program that reads a sequence of numbers.
+        //If the number is even, print its absolute value in the following format: "The number is: {number}"
+        // and terminate the program.
+        // If the number is odd, print "Please write an even number." and continue reading numbers. 
+
+        int input;
+
+        while (true)
+        {
+            input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("The number is: {0}", Math.Abs(input));
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please write an even number.");
+                continue;
+            }
+
+        }
+
+    }
+
+}
---
+++ 13.Holidays/Program.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * 13. Holidays Between Two Dates
+ */
+
+using System;
+
+namespace _13.Holidays
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+    }
+
+}
